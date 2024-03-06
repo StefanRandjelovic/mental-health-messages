fb0107451ccd3ec4080da8cc2cb1d417c11808ed
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-06 22:39:19 +0100

    Add messages translated into Serbian into a new array.

--- src/info/informations-array.js
+++ src/info/informations-array.js
@@ -25,4 +25,30 @@ const informationsArrayEng = [
   },
 ];
 
-export { informationsArrayEng };
+const informationsArraySrb = [
+  {
+    izreka:
+      "Baš u našim najmračnijim momentima se moramo fokusirati da bi videli svetlost.",
+    autor: "Aristotel",
+  },
+  {
+    izreka:
+      "Ne morate kontrolisati svoje misli. Samo morate da prestanete da im dozvoljavate da one upravljaju vama. ",
+    autor: "Den Milman",
+  },
+  {
+    izreka:
+      "Prigrlite svoje najteže trenutke kao deo prirodnog ciklusa. Život nije linearan, više je cikličan. Postoji doba i vreme za sve.",
+    autor: "Sonia Mao",
+  },
+  {
+    izreka: 'Kada kažete "Da" drugima, potrudite se da ne kažete "Ne" sebi',
+    autor: "Paulo Koeljo",
+  },
+  {
+    izreka: "Najgora vrsta usamljenosti je kada nismo načisto sami sa sobom.",
+    autor: "Mark Tven",
+  },
+];
+
+export { informationsArrayEng, informationsArraySrb };
