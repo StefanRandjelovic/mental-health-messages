b9c54ebaa2f0b750946fd4820bd27ba82d5bf79d
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-08 21:40:58 +0100

    Add additional sayings.

--- src/info/informations-array.js
+++ src/info/informations-array.js
@@ -23,31 +23,123 @@ const informationsArrayEng = [
     saying: "The worst loneliness is to not be comfortable with yourself.",
     author: "Mark Twain",
   },
+  {
+    saying: "Talk to yourself as you would someone you love.",
+    author: "Brene Brown",
+  },
+  {
+    saying:
+      "Put yourself at the top of your to-do list every single day and the rest will fall into place.",
+    author: "Unknown",
+  },
+  {
+    saying: "When things change inside you, things change around you.",
+    author: "Unknown",
+  },
+  {
+    saying:
+      "When you stop worrying about what you cannot control, you have time to change the things you can control. And that changes everything.",
+    author: "Marcandangel",
+  },
+  {
+    saying:
+      "Happiness is letting go of what you think your life is supposed to look like right now, and appreciating it for everything that it is.",
+    author: "Marcandangel",
+  },
+  {
+    saying:
+      "Sadness is a feeling. It is not who I am. Just like anxiety. Even happiness. I am not any of my feelings. I am the one who feels them.",
+    author: "sistercody",
+  },
+  {
+    saying:
+      "Mental health is not a destination, but a process. It's about how you drive, not where you're going.",
+    author: "HealthyPlace.com",
+  },
+  {
+    saying:
+      "You can't wait until life isn't hard anymore to decide to be happy.",
+    author: "Nightbirde",
+  },
+  {
+    saying:
+      "It is a miserable existence to be alone in your dread, allow yourself the life you crave and deserve",
+    author: "S.R.",
+  },
+  {
+    saying:
+      "Mental health problems don't define who you are. They are something you experience. You walk in the rain and you feel the rain, but, importantly, YOU ARE NOT THE RAIN.",
+    author: "Matt Haig",
+  },
 ];
-
 const informationsArraySrb = [
   {
     izreka:
-      "Baš u našim najmračnijim momentima se moramo fokusirati da bi videli svetlost.",
-    autor: "Aristotel",
+      "Баш у нашим најмрачнијим моментима се морамо фокусирати да би видели светлост.",
+    autor: "Аристотел",
   },
   {
     izreka:
-      "Ne morate kontrolisati svoje misli. Samo morate da prestanete da im dozvoljavate da one upravljaju vama. ",
-    autor: "Den Milman",
+      "Не морате контролисати своје мисли. Само морате да престанете да им дозвољавате да оне управљају вама. ",
+    autor: "Ден Милмен",
   },
   {
     izreka:
-      "Prigrlite svoje najteže trenutke kao deo prirodnog ciklusa. Život nije linearan, više je cikličan. Postoji doba i vreme za sve.",
-    autor: "Sonia Mao",
+      "Пригрлите своје најтеже тренутке као део природног циклуса. Живот није линеаран, више је цикличан. Постоји доба и време за све.",
+    autor: "Сониа Мао",
   },
   {
-    izreka: 'Kada kažete "Da" drugima, potrudite se da ne kažete "Ne" sebi',
-    autor: "Paulo Koeljo",
+    izreka: 'Kaда кажете "Да" другима, потрудите се да не кажете "Не" себи.',
+    autor: "Пауло Коељо",
   },
   {
-    izreka: "Najgora vrsta usamljenosti je kada nismo načisto sami sa sobom.",
-    autor: "Mark Tven",
+    izreka: "Најгора врста усамљености је када нисмо начисто сами са собом.",
+    autor: "Марк Твен",
+  },
+  { izreka: "Причај са собом као са неким кога волиш.", autor: "Брене Браун" },
+  {
+    izreka:
+      "Постави себе на врх своје за-урадити листе сваки дан, и остатле ствари ће лећи на своје место.",
+    autor: "Непознат аутор",
+  },
+  {
+    izreka: "Када се ствари промене у теби, ствари ће се променити и око тебе.",
+    autor: "Непознат аутор",
+  },
+  {
+    izreka:
+      "Када престанете да бринете о стварима које не можете да контролишете, имаћете времена да промените ствари које можете да контролишете. И то мења све.",
+    autor: "Маркандангел",
+  },
+  {
+    izreka:
+      "Срећа је када пустимо оно шта мислимо да наш живот треба да изгледа тренутно и почнемо да га ценимо за све оно што он тренутно јесте.",
+    autor: "Маркандангел",
+  },
+  {
+    izreka:
+      "Туга је осећање. Она није оно што јесам, исто као страх или срећа. Нисам ни једно од мојих осећања. Ја сам онај који их доживљава.",
+    autor: "систерцоди",
+  },
+  {
+    izreka:
+      "Ментално здравље није крајње одредиште, него процес. Ту метафорички ради о томе како возимо, а не куда смо кренули.",
+    autor: "HealthyPlace.com",
+  },
+  {
+    izreka:
+      "Не можеш чекати да живот престане бити тежак да би одлучио да будеш срећан.",
+    autor: "Најтбирде",
+  },
+  {
+    izreka:
+      "Мучна је то егзистенција када си сам у својој тескоби, дозволи себи живот за којим жудиш и који заслужујеш.",
+    autor: "С.Р.",
+  },
+  {
+    izreka:
+      "Проблеми са менталним здрављем не одређују оно шта смо. Они су само оно што искушавамо. Када ходаш по киши осећаш кишу, али важно је сетити се, ДА ТИ НИСИ КИША.",
+    autor: "Мет Хејг",
   },
 ];
 
